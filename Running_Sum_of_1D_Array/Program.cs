0ee7e6f7ebbf62f6817857adb0359809d8e5a80f
Admin <Admin@DESKTOP-DFAIJ8K>
2024-12-26 17:03:46 +0200

    Add project files.

---
+++ Running_Sum_of_1D_Array/Program.cs
@@ -0,0 +1,13 @@
+﻿new Solution().RunningSum([1, 2, 3, 4]);
+public class Solution
+{
+	public int[] RunningSum(int[] nums)
+	{
+		for (int i = 1; i < nums.Length; i++)
+		{
+			nums[i] = nums[i - 1] + nums[i];
+		}
+
+		return nums;
+	}
+}
